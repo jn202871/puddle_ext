ef6af55956b6d97ba6118f228e301af6877283c4
Jay Nash <jn202871@gmail.com>
2024-07-11 21:51:21 -0400

    changed version/gitignore

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='puddle_ext',
-    version='1.0',
+    version='1.1',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
